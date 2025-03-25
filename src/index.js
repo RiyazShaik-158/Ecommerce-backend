265422e3333b416f837b612015357b9a1b5493aa
RiyazShaik-158 <riyaz.shaik.1481@gmail.com>
2025-03-25 23:42:58 +0530

    feat(added frontend url): added initial frontend localhost address

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors("http://localhost:5173"));
 const port = 8900;
 
 app.use("/users", UserRouter);
